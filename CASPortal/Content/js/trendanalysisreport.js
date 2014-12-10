a26da2a1fd8fd078002ffb5cfa33ac18611c9e10
arefinforbd <arefinforbd@yahoo.com>
2014-12-10 17:24:25 +1100

    Modified

--- CASPortal/Content/js/trendanalysisreport.js
+++ CASPortal/Content/js/trendanalysisreport.js
@@ -320,10 +320,6 @@ $(function () {
 
         return false;
     });
-
-    $("#canvas").mouseover(function (e) {
-        e.preventDefault();
-    })
 });
 
 function Loading() {
@@ -344,9 +340,168 @@ function LoadingComplete() {
     $("#btnReset").removeAttr("disabled");
 }
 
+function LoadLineChart(data) {
+
+    var html = "";
+    var labels = [];
+    dataset = [];
+    yaxisvalues = [];
+
+    for (var index = 0; index < data.Lines.length; index++) {
+        if (index % data.Lines[0].Count == 0 && index > 0) {
+            LineColor();
+            dataset.push({
+                fillColor: "rgba(255,255,255,0)",
+                strokeColor: "rgba(" + lineColor + ",0.6)",
+                pointColor: "rgba(" + lineColor + ",0.6)",
+                pointStrokeColor: "#fff",
+                pointHighlightFill: "#fff",
+                pointHighlightStroke: "rgba(" + lineColor + ",1)",
+                data: yaxisvalues
+            });
+            yaxisvalues = [];
+
+            html += '<div style="background-color: rgba(' + lineColor + ',0.8);width: 12px; height: 12px; float: left; margin-top: 2px;"></div>';
+            html += '<div style="font-size: 12px; margin-left: 5px; float: left; margin-right: 5px;">' + data.Lines[index - 1].label + '</div>';
+        }
+        yaxisvalues.push([data.Lines[index].lineValue]);
+    }
+
+    LineColor();
+    dataset.push({
+        fillColor: "rgba(255,255,255,0)",
+        strokeColor: "rgba(" + lineColor + ",0.6)",
+        pointColor: "rgba(" + lineColor + ",0.6)",
+        pointStrokeColor: "#fff",
+        pointHighlightFill: "#fff",
+        pointHighlightStroke: "rgba(" + lineColor + ",1)",
+        data: yaxisvalues
+    });
+
+    html += '<div style="background-color: rgba(' + lineColor + ',0.8);width: 12px; height: 12px; float: left; margin-top: 2px;"></div>';
+    html += '<div style="font-size: 12px; margin-left: 5px; float: left; margin-right: 5px;">' + data.Lines[index - 1].label + '</div>';
+    $("#divLineLegend").html(html);
+
+    for (var index = 0; index < data.Lines[0].Count; index++) {
+        labels.push([data.Lines[index].DateLabel]);
+    }
+
+    var lineChartData = {
+        labels: labels,
+        datasets: dataset
+    }
+    $("#tooltip").remove();
+
+    $("#divLinePanel").show();
+    var ctx = document.getElementById("flot-line-chart").getContext("2d");
+    window.myLine = new Chart(ctx).Line(lineChartData, {
+        responsive: false
+    });
+    $("#flot-line-chart").height($("#divLinePanel").height() - 80);
+}
+
+function LoadPieChart(data) {
+
+    var options = {
+        series: {
+            pie: {
+                show: true,
+                radius: 0.7,
+                combine: {
+                    color: '#999'
+                }
+            }
+        },
+        legend: {
+            show: false
+        },
+        grid: {
+            hoverable: true
+        },
+        tooltip: true,
+        tooltipOpts: {
+            content: "%p.0%, %s", // show percentages, rounding to 2 decimal places
+            shifts: {
+                x: 20,
+                y: 0
+            },
+            defaultTheme: false
+        }
+    };
+
+    $("#divPiePanel").show();
+    $.plot($("#flot-pie-chart"), data.Pies, options);
+}
+
+function LoadBarChart(data) {
+
+    dataset = [];
+    yaxisvalues = [];
+
+    var innerindex = 0;
+    var xvalues = [];
+    var parsedData = data.Bars;
+    var colCount = 0;
+
+    //Creating labels for x-axis
+    for (var loopindex = 0; loopindex < data.Bars.length; loopindex++) {
+        $.each(parsedData[loopindex], function (colName, value) {
+            if (colName == "DateLabel") {
+                xvalues.push([loopindex, value]);
+            }
+        });
+    }
+
+    //Counting the number of columns returned from server
+    $.each(parsedData[0], function (colName, value) {
+        if (colName == "DateLabel") {
+            colCount++;
+        }
+    });
+
+    innerindex = 0;
+    for (var loopindex = 0; loopindex < colCount; loopindex++) {
+        $.each(data.Bars[loopindex], function (colName, value) {
+            if (colName != "DateLabel") {
+
+                for (var index = 0; index < data.Bars.length; index++) {
+                    yaxisvalues.push([index, data.Bars[index][colName]]);
+                }
+
+                dataset.push({
+                    label: colName,
+                    bars: { order: innerindex },
+                    data: yaxisvalues
+                });
+                yaxisvalues = [];
+                innerindex++;
+            }
+        });
+    }
+
+    $("#divBarPanel").show();
+    $.plot($("#flot-bar-chart"), dataset, {
+        series: {
+            bars: {
+                show: true,
+                barWidth: 0.13,
+                align: 'center'
+            }
+        },
+        grid: {
+            hoverable: true
+        },
+        valueLabels: {
+            show: true
+        },
+        xaxis: {
+            ticks: xvalues
+        }
+    });
+}
+
 $("#btnPreview").click(function () {
 
-    var yKeys = [];
     var labels = [];
     var strChartType = "All Charts";
 
@@ -367,179 +522,19 @@ $("#btnPreview").click(function () {
             if (data != null) {
                 //$('#jstree').jstree("deselect_all");
 
-                var html = "";
-                yKeys = [];
-                labels = [];
-                yaxisvalues = [];
-                dataset = [];
-
                 //LINE Chart
                 if (($("#spanChartType").html() == strChartType || $("#spanChartType").html() == "LINE")) {
-                    labels = [];
-                    dataset = [];
-                    html = "";
-                    for (var index = 0; index < data.Lines.length; index++) {
-                        if (index % data.Lines[0].Count == 0 && index > 0) {
-                            LineColor();
-                            dataset.push({
-                                fillColor: "rgba(255,255,255,0)",
-                                strokeColor: "rgba(" + lineColor + ",0.6)",
-                                pointColor: "rgba(" + lineColor + ",0.6)",
-                                pointStrokeColor: "#fff",
-                                pointHighlightFill: "#fff",
-                                pointHighlightStroke: "rgba(" + lineColor + ",1)",
-                                //label: data.Lines[index - 1].label,
-                                data: yaxisvalues
-                            });
-                            yaxisvalues = [];
-
-                            html += '<div style="background-color: rgba(' + lineColor + ',0.8);width: 12px; height: 12px; float: left; margin-top: 2px;"></div>';
-                            html += '<div style="font-size: 12px; margin-left: 5px; float: left; margin-right: 5px;">' + data.Lines[index - 1].label + '</div>';
-                        }
-                        yaxisvalues.push([data.Lines[index].lineValue]);
-                    }
-
-                    LineColor();
-                    dataset.push({
-                        fillColor: "rgba(255,255,255,0)",
-                        strokeColor: "rgba(" + lineColor + ",0.6)",
-                        pointColor: "rgba(" + lineColor + ",0.6)",
-                        pointStrokeColor: "#fff",
-                        pointHighlightFill: "#fff",
-                        pointHighlightStroke: "rgba(" + lineColor + ",1)",
-                        //label: data.Lines[index - 1].label,
-                        data: yaxisvalues
-                    });
-
-                    html += '<div style="background-color: rgba(' + lineColor + ',0.8);width: 12px; height: 12px; float: left; margin-top: 2px;"></div>';
-                    html += '<div style="font-size: 12px; margin-left: 5px; float: left; margin-right: 5px;">' + data.Lines[index - 1].label + '</div>';
-                    $("#divLineLegend").html(html);
-
-                    for (var index = 0; index < data.Lines[0].Count; index++) {
-                        labels.push([data.Lines[index].DateLabel]);
-                    }
-
-                    var lineChartData = {
-                        labels: labels,
-                        datasets: dataset
-                    }
-
-                    $("#divLinePanel").show();
-                    var ctx = document.getElementById("flot-line-chart").getContext("2d");
-                    window.myLine = new Chart(ctx).Line(lineChartData, {
-                        responsive: false
-                    });
-                    $("#flot-line-chart").height($("#divLinePanel").height() - 80);
+                    LoadLineChart(data);
                 }
-
-                html = "";
-                yKeys = [];
-                labels = [];
-                yaxisvalues = [];
-                dataset = [];
 
                 //PIE Chart
                 if (($("#spanChartType").html() == strChartType || $("#spanChartType").html() == "PIE")) {
-                    var options = {
-                        series: {
-                            pie: {
-                                show: true,
-                                radius: 0.7,
-                                combine: {
-                                    color: '#999'
-                                }
-                            }
-                        },
-                        legend: {
-                            show: false
-                        },
-                        grid: {
-                            hoverable: true
-                        },
-                        tooltip: true,
-                        tooltipOpts: {
-                            content: "%p.0%, %s", // show percentages, rounding to 2 decimal places
-                            shifts: {
-                                x: 20,
-                                y: 0
-                            },
-                            defaultTheme: false
-                        }
-                    };
-
-                    $("#divPiePanel").show();
-                    $.plot($("#flot-pie-chart"), data.Pies, options);
-                    
+                    LoadPieChart(data);                    
                 }
-
-                html = "";
-                yKeys = [];
-                labels = [];
-                yaxisvalues = [];
-                dataset = [];
 
                 //BAR Chart
                 if (($("#spanChartType").html() == strChartType || $("#spanChartType").html() == "BAR")) {
-
-                    var innerindex = 0;
-                    var xvalues = [];
-                    var parsedData = data.Bars;
-                    var colCount = 0;
-
-                    for (var loopindex = 0; loopindex < data.Bars.length; loopindex++) {
-                        $.each(parsedData[loopindex], function (colName, value) {
-                            if (colName == "DateLabel") {
-                                xvalues.push([loopindex, value]);
-                            }
-                        });
-                    }
-
-                    $.each(parsedData[0], function (colName, value) {
-                        if (colName == "DateLabel") {
-                            innerindex++;
-                            colCount++;
-                        }
-                    });
-
-                    innerindex = 0;
-                    for (var loopindex = 0; loopindex < colCount; loopindex++) {
-                        $.each(data.Bars[loopindex], function (colName, value) {
-                            if (colName != "DateLabel") {
-
-                                for (var index = 0; index < data.Bars.length; index++) {
-                                    yaxisvalues.push([index, data.Bars[index][colName]]);
-                                }
-
-                                dataset.push({
-                                    label: colName,
-                                    bars: { order: innerindex },
-                                    data: yaxisvalues
-                                });
-                                yaxisvalues = [];
-                                innerindex++;
-                            }
-                        });
-                    }
-
-                    $("#divBarPanel").show();
-                    $.plot($("#flot-bar-chart"), dataset, {
-                        series: {
-                            bars: {
-                                show: true,
-                                barWidth: 0.13,
-                                align: 'center'
-                            }
-                        },
-                        grid: {
-                            hoverable: true
-                        },
-                        valueLabels: {
-                            show: true
-                        },
-                        xaxis: {
-                            ticks: xvalues
-                        }
-                    });
+                    LoadBarChart(data);
                 }
 
                 LoadingComplete();
@@ -587,6 +582,7 @@ $("#btnReset").click(function () {
     $("#ddlConracts").hide();
     $("#jstree").hide();
     $("#divRightSide").hide();
+    $("#divLeftSide").css("border-right", "");
 });
 
 $("#btnLineDownload").click(function () {
